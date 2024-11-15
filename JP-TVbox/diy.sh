7933e033e0153bd01d53963eb90ccf1d7bd8a6a0
jumbotsang <xiaotiaody@gmail.com>
2024-11-15 17:30:37 +0800

    Update diy.sh

--- JP-TVbox/diy.sh
+++ JP-TVbox/diy.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
 
 # Default IP
-sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.3/g' package/base-files/files/bin/config_generate
 
-# Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall
 
 # Add packages
-git clone https://github.com/nantayo/My-Pkg package/my-pkg
-git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+#添加科学上网源
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/ophub/luci-app-amlogic package/amlogic
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
+
+
+#删除库中的插件，使用自定义源中的包。
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/applications/openwrt-passwall
