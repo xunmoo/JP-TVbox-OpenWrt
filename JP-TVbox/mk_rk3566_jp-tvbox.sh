0b76b508b42681dfbf6e0116309cf9adf19d4ef5
jumbotsang <xiaotiaody@gmail.com>
2024-11-15 12:42:32 +0800

    Update mk_rk3566_jp-tvbox.sh

--- JP-TVbox/mk_rk3566_jp-tvbox.sh
+++ JP-TVbox/mk_rk3566_jp-tvbox.sh
@@ -1,53 +1,58 @@
 #!/bin/bash
 
-echo "========================= begin $0 ==========================="
+echo "========================= begin $0 ================="
 source make.env
 source public_funcs
 init_work_env
 
-# 盒子型号识别参数 
-PLATFORM=amlogic
-SOC=s905d
-BOARD=n1
+# 默认是否开启软件FLOWOFFLOAD
+SW_FLOWOFFLOAD=0
+# 默认是否开启硬件FLOWOFFLOAD
+HW_FLOWOFFLOAD=0
+# 默认是否开启SFE
+SFE_FLOW=1
 
-# 让N1一直有wifi可用，以减少抱怨
-# 5.10(及以上)内核是否启用wifi  1:启用 0:禁用
-ENABLE_WIFI_K510=0
-
+PLATFORM=rockchip
+SOC=rk3566
+BOARD=jp-tvbox
 SUBVER=$1
+
+#export KERNEL_VERSION=$RK35XX_KERNEL_VERSION
 
 # Kernel image sources
 ###################################################################
-KERNEL_TAGS="stable"
-KERNEL_BRANCHES="mainline:all:>=:5.4"
+KERNEL_TAGS="rk35xx"
+KERNEL_BRANCHES="bsp:rk35xx:>=:5.10 mainline:all:>=:6.1"
 MODULES_TGZ=${KERNEL_PKG_HOME}/modules-${KERNEL_VERSION}.tar.gz
 check_file ${MODULES_TGZ}
 BOOT_TGZ=${KERNEL_PKG_HOME}/boot-${KERNEL_VERSION}.tar.gz
 check_file ${BOOT_TGZ}
-DTBS_TGZ=${KERNEL_PKG_HOME}/dtb-amlogic-${KERNEL_VERSION}.tar.gz
+DTBS_TGZ=${KERNEL_PKG_HOME}/dtb-rockchip-${KERNEL_VERSION}.tar.gz
 check_file ${DTBS_TGZ}
-K510=$(get_k510_from_boot_tgz "${BOOT_TGZ}" "vmlinuz-${KERNEL_VERSION}")
-export K510
-###########################################################################
+###################################################################
 
-# Openwrt root 源文件
+# Openwrt 
 OPWRT_ROOTFS_GZ=$(get_openwrt_rootfs_archive ${PWD})
 check_file ${OPWRT_ROOTFS_GZ}
 echo "Use $OPWRT_ROOTFS_GZ as openwrt rootfs!"
 
-# 目标镜像文件
+# Target Image
 TGT_IMG="${WORK_DIR}/openwrt_${SOC}_${BOARD}_${OPENWRT_VER}_k${KERNEL_VERSION}${SUBVER}.img"
 
-# 补丁和脚本
-###########################################################################
-KMOD="${PWD}/files/kmod"
-KMOD_BLACKLIST="${PWD}/files/kmod_blacklist"
-MAC_SCRIPT2="${PWD}/files/find_macaddr.pl"
-MAC_SCRIPT3="${PWD}/files/inc_macaddr.pl"
+# patches、scripts
+####################################################################
 CPUSTAT_SCRIPT="${PWD}/files/cpustat"
 CPUSTAT_SCRIPT_PY="${PWD}/files/cpustat.py"
 INDEX_PATCH_HOME="${PWD}/files/index.html.patches"
 GETCPU_SCRIPT="${PWD}/files/getcpu"
+KMOD="${PWD}/files/kmod"
+KMOD_BLACKLIST="${PWD}/files/kmod_blacklist"
+
+FIRSTRUN_SCRIPT="${PWD}/files/first_run.sh"
+
+DAEMON_JSON="${PWD}/files/rk3566/daemon.json"
+
+TTYD="${PWD}/files/ttyd"
 FLIPPY="${PWD}/files/scripts_deprecated/flippy_cn"
 BANNER="${PWD}/files/banner"
 
@@ -56,13 +61,10 @@ FMW_HOME="${PWD}/files/firmware"
 SMB4_PATCH="${PWD}/files/smb4.11_enable_smb1.patch"
 SYSCTL_CUSTOM_CONF="${PWD}/files/99-custom.conf"
 
-# 20200930 add
-SND_MOD="${PWD}/files/s905d/snd-meson-gx"
-DAEMON_JSON="${PWD}/files/s905d/daemon.json"
+# 20200709 add
+COREMARK="${PWD}/files/coremark.sh"
 
-# 20201006 add
-FORCE_REBOOT="${PWD}/files/s905d/reboot"
-# 20201017 add
+# 20201024 add
 BAL_ETH_IRQ="${PWD}/files/balethirq.pl"
 # 20201026 add
 FIX_CPU_FREQ="${PWD}/files/fixcpufreq.pl"
@@ -72,18 +74,11 @@ SYSFIXTIME_PATCH="${PWD}/files/sysfixtime.patch"
 SSL_CNF_PATCH="${PWD}/files/openssl_engine.patch"
 
 # 20201212 add
-BAL_CONFIG="${PWD}/files/s905d/balance_irq"
-CPUFREQ_INIT="${PWD}/files/s905d/cpufreq"
-
-# 20210302 modify
-FIP_HOME="${PWD}/files/meson_btld/with_fip/s905d"
-UBOOT_WITH_FIP="${FIP_HOME}/n1-u-boot.bin.sd.bin"
-UBOOT_WITHOUT_FIP_HOME="${PWD}/files/meson_btld/without_fip"
-UBOOT_WITHOUT_FIP="u-boot-n1.bin"
+BAL_CONFIG="${PWD}/files/rk3566/jp-tvbox/balance_irq"
 
 # 20210307 add
 SS_LIB="${PWD}/files/ss-glibc/lib-glibc.tar.xz"
-SS_BIN="${PWD}/files/ss-glibc/armv8a_crypto/ss-bin-glibc.tar.xz"
+SS_BIN="${PWD}/files/ss-glibc/armv8.2a_crypto/ss-bin-glibc.tar.xz"
 JQ="${PWD}/files/jq"
 
 # 20210330 add
@@ -91,75 +86,67 @@ DOCKERD_PATCH="${PWD}/files/dockerd.patch"
 
 # 20200416 add
 FIRMWARE_TXZ="${PWD}/files/firmware_armbian.tar.xz"
-BOOTFILES_HOME="${PWD}/files/bootfiles/amlogic"
+BOOTFILES_HOME="${PWD}/files/bootfiles/rockchip/rk3566/jp-tvbox"
 GET_RANDOM_MAC="${PWD}/files/get_random_mac.sh"
+BOOTLOADER_IMG="${PWD}/files/rk3566/jp-tvbox/bootloader.bin"
 
 # 20210618 add
 DOCKER_README="${PWD}/files/DockerReadme.pdf"
 
 # 20210704 add
 SYSINFO_SCRIPT="${PWD}/files/30-sysinfo.sh"
+FORCE_REBOOT="${PWD}/files/rk3566/reboot"
 
 # 20210923 add
-OPENWRT_INSTALL="${PWD}/files/openwrt-install-amlogic"
-OPENWRT_UPDATE="${PWD}/files/openwrt-update-amlogic"
 OPENWRT_KERNEL="${PWD}/files/openwrt-kernel"
 OPENWRT_BACKUP="${PWD}/files/openwrt-backup"
-
-# 20211019 add
-FIRSTRUN_SCRIPT="${PWD}/files/first_run.sh"
-
-# 20211020 add
-BTLD_BIN="${PWD}/files/s905d/u-boot-2015-phicomm-n1.bin"
-
-# 20211024 add
-MODEL_DB="${PWD}/files/amlogic_model_database.txt"
+OPENWRT_UPDATE="${PWD}/files/openwrt-update-rockchip"
 # 20211214 add
 P7ZIP="${PWD}/files/7z"
 # 20211217 add
 DDBR="${PWD}/files/openwrt-ddbr"
 # 20220225 add
-SSH_CIPHERS="aes128-gcm@openssh.com,aes256-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr,chacha20-poly1305@openssh.com"
-SSHD_CIPHERS="aes128-gcm@openssh.com,aes256-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr"
-###########################################################################
+SSH_CIPHERS="aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr,chacha20-poly1305@openssh.com"
+SSHD_CIPHERS="aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr"
+# 20220927 add
+BOARD_HOME="${PWD}/files/rk3566/jp-tvbox/board.d"
+# 20221001 add
+MODULES_HOME="${PWD}/files/rk3566/modules.d"
+# 20221123 add
+BOARD_MODULES_HOME="${PWD}/files/rk3566/jp-tvbox/modules.d"
+# 20221013 add
+WIRELESS_CONFIG="${PWD}/files/rk3566/jp-tvbox/wireless"
+####################################################################
 
 check_depends
 
-SKIP_MB=4
+SKIP_MB=16
 BOOT_MB=256
 ROOTFS_MB=960
-SIZE=$((SKIP_MB + BOOT_MB + ROOTFS_MB))
+SIZE=$((SKIP_MB + BOOT_MB + ROOTFS_MB + 1))
 create_image "$TGT_IMG" "$SIZE"
-create_partition "$TGT_DEV" "msdos" "$SKIP_MB" "$BOOT_MB" "fat32" "0" "-1" "btrfs"
-make_filesystem "$TGT_DEV" "B" "fat32" "BOOT" "R" "btrfs" "ROOTFS"
-mount_fs "${TGT_DEV}p1" "${TGT_BOOT}" "vfat"
+create_partition "$TGT_DEV" "gpt" "$SKIP_MB" "$BOOT_MB" "ext4" "0" "-1" "btrfs"
+make_filesystem "$TGT_DEV" "B" "ext4" "EMMC_BOOT" "R" "btrfs" "EMMC_ROOTFS1"
+mount_fs "${TGT_DEV}p1" "${TGT_BOOT}" "ext4"
 mount_fs "${TGT_DEV}p2" "${TGT_ROOT}" "btrfs" "compress=zstd:${ZSTD_LEVEL}"
 echo "创建 /etc 子卷 ..."
 btrfs subvolume create $TGT_ROOT/etc
 extract_rootfs_files
-extract_amlogic_boot_files
+extract_rockchip_boot_files
 
 echo "修改引导分区相关配置 ... "
 cd $TGT_BOOT
-rm -f uEnv.ini
-cat > uEnv.txt <<EOF
-LINUX=/zImage
-INITRD=/uInitrd
-
-# 下列 dtb，用到哪个就把哪个的#删除，其它的则加上 # 在行首
-
-# 用于 Phicomm N1
-FDT=/dtb/amlogic/meson-gxl-s905d-phicomm-n1.dtb
-
-# 用于 Phicomm N1 (thresh)
-#FDT=/dtb/amlogic/meson-gxl-s905d-phicomm-n1-thresh.dtb
-
-APPEND=root=UUID=${ROOTFS_UUID} rootfstype=btrfs rootflags=compress=zstd:${ZSTD_LEVEL} console=ttyAML0,115200n8 console=tty0 no_console_suspend consoleblank=0 fsck.fix=yes fsck.repair=yes net.ifnames=0 cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory swapaccount=1
+sed -e '/rootdev=/d' -i armbianEnv.txt
+sed -e '/rootfstype=/d' -i armbianEnv.txt
+sed -e '/rootflags=/d' -i armbianEnv.txt
+cat >> armbianEnv.txt <<EOF
+rootdev=UUID=${ROOTFS_UUID}
+rootfstype=btrfs
+rootflags=compress=zstd:${ZSTD_LEVEL}
 EOF
-
-echo "uEnv.txt -->"
+echo "armbianEnv.txt -->"
 echo "==============================================================================="
-cat uEnv.txt
+cat armbianEnv.txt
 echo "==============================================================================="
 echo
 
@@ -169,17 +156,22 @@ copy_supplement_files
 extract_glibc_programs
 adjust_docker_config
 adjust_openssl_config
+adjust_qbittorrent_config
 adjust_getty_config
 adjust_samba_config
+adjust_nfs_config "mmcblk0p4"
 adjust_openssh_config
+adjust_openclash_config
 use_xrayplug_replace_v2rayplug
 create_fstab_config
+adjust_turboacc_config
+adjust_ntfs_config
 adjust_mosdns_config
 patch_admin_status_index_html
 adjust_kernel_env
 copy_uboot_to_fs
 write_release_info
-write_banner 
+write_banner
 config_first_run
 create_snapshot "etc-000"
 write_uboot_to_disk
