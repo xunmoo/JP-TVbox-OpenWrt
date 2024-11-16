fb741c5bc67e4199cb1e71f841b4929d3db31e66
xunmoo <23499618+xunmoo@users.noreply.github.com>
2024-11-16 10:32:58 +0800

    Update diy.sh

--- JP-TVbox/diy.sh
+++ JP-TVbox/diy.sh
@@ -3,15 +3,11 @@
 # Default IP
 sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
 
-# Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall
-
 # Add packages
 #添加科学上网源
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth 1 https://github.com/ophub/luci-app-amlogic package/amlogic
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
-
 
 #删除库中的插件，使用自定义源中的包。
 rm -rf feeds/luci/themes/luci-theme-argon
